56cfa5939e3e7d094fc4010be48cbc7864b1c45d
Nathan <nathanmercier@orange.fr>
2019-02-07 15:38:31 +0100

    project

---
+++ src/fr/project_j2ee/servlet/LoginServlet.java
@@ -0,0 +1,17 @@
+package fr.project_j2ee.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class LoginServlet extends HttpServlet {
+	
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
